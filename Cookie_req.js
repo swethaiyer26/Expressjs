b0d1d09b3082eb82885c4cd6540decaa36960886
Swetha Janardhan <swethaiyer26@gmail.com>
2020-10-05 19:01:00 +0530

    Delete Cookie_req.js

--- Cookie_req.js
+++
@@ -1,9 +0,0 @@
-var express=require('express');
-var express=require('express');
-var app=express();
-app.get('/',function(req,res){
-    req.cookie('name','express').send('cookie set');
-    console.log('Cookies',req.cookies);
-  
-});
-app.listen(3000);
